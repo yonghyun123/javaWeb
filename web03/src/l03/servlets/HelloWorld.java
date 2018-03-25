3604e6ce054bbe26153b91691bebc350fe506973
yonghyun123 <model9960@naver.com>
2018-03-25 22:05:24 +0900

    서블릿 프로그래밍 tomcat과 서블릿의 관계

--- web03/src/l03/servlets/HelloWorld.java
+++ web03/src/l03/servlets/HelloWorld.java
@@ -2,25 +2,15 @@ package l03.servlets;
 
 import java.io.IOException;
 
-import javax.servlet.Servlet;
+import javax.servlet.GenericServlet;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 
-public class HelloWorld implements Servlet {
+public class HelloWorld extends GenericServlet {
 	ServletConfig config;
 	
-	@Override
-	public void init(ServletConfig config) throws ServletException {
-		System.out.println("init() 호출됨");
-		this.config = config;
-	}
-	
-	@Override
-	public void destroy() {
-		System.out.println("destroy() 호출됨");
-	}
 	
 	@Override
 	public void service(ServletRequest request, ServletResponse response)
@@ -28,15 +18,4 @@ public class HelloWorld implements Servlet {
 		System.out.println("service() 호출됨");
 	}
 
-	@Override
-	public ServletConfig getServletConfig() {
-		System.out.println("getServletConfig() 호출됨");
-		return this.config;
-	}
-
-	@Override
-	public String getServletInfo() {
-		System.out.println("getServletInfo() 호출됨");
-		return "version=1.0;author=eomjinyoung;copyright=eomjinyoung 2013";
-	}
 }
