5b848972b33e8bff3a1f5ccd592cb5375ba11ee4
yonghyun123 <model9960@naver.com>
2018-06-01 11:35:46 +0900

    servlet 이용

---
+++ exam31/src/exam/TenServlet.java
@@ -0,0 +1,50 @@
+package exam;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TenServlet
+ */
+@WebServlet("/Ten")
+public class TenServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TenServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("tex/html; charset=utf-8");
+		PrintWriter out = response.getWriter();
+		
+		out.print("<h1>1에서 10까지 출력</h1>");
+		for(int i = 1; i <= 10; i++){
+			out.print(i+"<br>");
+		}
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
