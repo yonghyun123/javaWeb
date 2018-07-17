8e664486621d9a3b3cc20429c9f6bf4c674ac072
yonghyun123 <model9960@naver.com>
2018-07-17 18:50:11 +0900

    spring annotation config 설정방법

--- diexam01/src/main/java/kr/or/connect/diexam01/Car.java
+++ diexam01/src/main/java/kr/or/connect/diexam01/Car.java
@@ -1,11 +1,16 @@
 package kr.or.connect.diexam01;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Car {
-	
 	private Engine v8;
+	
 	public Car(){
 		System.out.println("Car 생성자");
 	}
+
 	public void setEngine(Engine e){
 		v8 = e;
 	}
@@ -13,10 +18,10 @@ public class Car {
 		System.out.println("엔진을 이용하여 달립니다");
 		v8.exec();
 	}
-	
+
 	public static void main(String[] args){
-		Engine e = new Engine();
 		Car car = new Car();
+		Engine e = new Engine();
 		car.setEngine(e);
 		car.run();
 	}
