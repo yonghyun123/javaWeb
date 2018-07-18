c86eee44c54b83d0f95db6846a8f754c64c3df34
yonghyun123 <model9960@naver.com>
2018-07-18 17:38:41 +0900

    spring jdbc connect

---
+++ daoexam/src/main/java/kr/or/connect/daoexam/config/DBConfig.java
@@ -0,0 +1,10 @@
+package kr.or.connect.daoexam.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+public class DBConfig {
+
+}
